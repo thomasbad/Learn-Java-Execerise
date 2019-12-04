af3ca894f37ff3c12996782dda3c1a1b95636988
Hon Ho <mkaa@MKAAdeMacBook-Pro.local>
2019-12-04 16:33:14 +0800

    4-Dec edit

--- test.java
+++ test.java
@@ -15,6 +15,5 @@ public class test{
         a.addItem();
         b.deleteItem();
         //TODO: why there is not need to do a.close() to close the mem slot and no res leak error?
-        //tets git commit
     }
 }

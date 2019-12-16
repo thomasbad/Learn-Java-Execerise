588f6e8abb896052a2a2c2460ff83892dc6e9fb7
mkaa <thomasgoodboy@gmail.com>
2019-12-16 20:08:11 +0800

    Create TwoArray.java

---
+++ Testing_Practice_Usage/TwoArray.java
@@ -0,0 +1,15 @@
+public class TwoArray {
+    public static void main(String args[]) {
+      int arr[][] = new int[2][2];
+      arr[0][0] = 1;
+      arr[0][1] = 2;
+      arr[1][0] = 3;
+      arr[1][1] = 4;
+      for (int i = 0; i < 2; i++){
+          for (int j = 0; j < 2; j++){
+              System.out.print(arr[i][j] + " ");
+          }
+          System.out.println();
+      }
+    }
+}

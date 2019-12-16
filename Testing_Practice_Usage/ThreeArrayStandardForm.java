468242bf2f6429dcbd0df06e5453e6406f4035e6
mkaa <thomasgoodboy@gmail.com>
2019-12-16 20:41:36 +0800

    Create ThreeArrayStandardForm.java

---
+++ Testing_Practice_Usage/ThreeArrayStandardForm.java
@@ -0,0 +1,11 @@
+public class ThreeArrayStandardForm {
+    public static void main(String args[]) {
+      int[][][] arr = new int[2][1][3]; //3D Array form
+      arr[0][0][0] = 10;
+      //arr[0][0][1] = 20;
+      //arr[0][0][2] = 30;
+      
+      System.out.println("arr[0][0][0]: " + arr[0][0][0]);
+      
+    }
+}

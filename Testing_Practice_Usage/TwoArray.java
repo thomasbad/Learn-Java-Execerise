3677cff053f9a62679917fe9ed19c929809ad4f2
mkaa <thomasgoodboy@gmail.com>
2019-12-16 20:12:14 +0800

    Update TwoArray.java

--- Testing_Practice_Usage/TwoArray.java
+++ Testing_Practice_Usage/TwoArray.java
@@ -6,10 +6,10 @@ public class TwoArray {
       arr[1][0] = 3;
       arr[1][1] = 4;
       for (int i = 0; i < 2; i++){
-          for (int j = 0; j < 2; j++){
+          for (int j = 0; j < 2; j++){ //Loop until all arr[i] is used
               System.out.print(arr[i][j] + " ");
           }
-          System.out.println();
+          System.out.println(); //print blank line after arr[i] changed
       }
     }
 }

4649aadd9d82e42a8f1c88afb3461c346ddde9b4
mkaa <thomasgoodboy@gmail.com>
2019-12-16 16:55:05 +0800

    swapnum practice update without comment in practice section

--- Testing_Practice_Usage/swapNumPractive.java
+++ Testing_Practice_Usage/swapNumPractive.java
@@ -1,16 +1,13 @@
-import java.util.Scanner;
-
-public class swapNumPractive {
-	public static void main(String args[]) {
-		Scanner input1 = new Scanner(System.in);
-		Scanner input2 = new Scanner(System.in);
-		System.out.print("Please input your 1st number: ");
-		int num1 = input1.nextInt();
-		System.out.print("Enter 2nd number: ");
-		int num2 = input2.nextInt();
-		System.out.print("The mutiply result of 1st number: " + num1 + " and the 2nd number: " + num2 + "is = ");
-		System.out.println(num1 * num2);
-		input1.close();
-		input2.close();
+public class swapNumPractive{
+	public static int swapNum(int num1, int num2) {
+		return num1;
 	}
+	
+	public static void main (String args[]) {
+		int a = 5, b = 10;
+		System.out.println("Before swapping: a, b = " + a + ", " + b);
+		a = swapNum(b, b = a);
+		System.out.println("After swapping: a, b = " + a + ", " + b);
+	}
+	
 }
---
+++ Testing_Practice_Usage/testingL2Q4.java
@@ -0,0 +1,22 @@
+public class testL2Q4{
+	public static void main (String args[]) {
+		int x = 1;
+		
+		System.out.println("Divided by 3 or 5:");
+		while (x <= 50) {
+			if (x % 3 == 0 || x % 5 == 0) {
+				System.out.print(x + " ");
+			}
+			x++;
+		}
+		System.out.println();
+		
+		System.out.println("Divided by both 3 and 5:");
+		while (x <= 50) {
+			if (x % 3 == 0 && x % 5 == 0) {
+				System.out.print(x + " ");
+			}
+			x++;
+		}
+	}
+}

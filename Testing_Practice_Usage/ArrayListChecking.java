51844b01d1cac2c2eb9a5f401fb1d3f4ed940726
mkaa <thomasgoodboy@gmail.com>
2019-12-16 19:55:50 +0800

    Create ArrayListChecking.java

---
+++ Testing_Practice_Usage/ArrayListChecking.java
@@ -0,0 +1,11 @@
+public class ArrayListChecking {
+    public static void main(String args[]) {
+      int arr[] = {11, 22, 33, 44};
+      System.out.println("Using arr.length method: " + arr.length);
+      System.out.println("Real Array Length: " + (arr.length - 1));
+      for (int i = 0; i < arr.length; i++){
+          System.out.print("Index = " + i);
+          System.out.println(", value = " + arr[i]);
+      }
+    }
+}

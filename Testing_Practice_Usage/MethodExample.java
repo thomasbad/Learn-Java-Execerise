490f0e95872cb79b6b23b048cb26f4bf56cc0010
mkaa <thomasgoodboy@gmail.com>
2019-12-16 20:47:02 +0800

    Create MethodExample.java

---
+++ Testing_Practice_Usage/MethodExample.java
@@ -0,0 +1,8 @@
+public class MethodExample {
+    public static void sum(int a, int b){
+        System.out.println("The sum is: " + (a + b));
+    }
+    public static void main(String args[]) {
+      sum(10, 20); //put args or perm inside () for the use of method
+    }
+}

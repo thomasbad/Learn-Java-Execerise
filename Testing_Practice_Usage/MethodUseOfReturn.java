a110ae0a28e5734446e54c939bd5517948374eef
mkaa <thomasgoodboy@gmail.com>
2019-12-16 20:56:14 +0800

    Create MethodUseOfReturn.java

---
+++ Testing_Practice_Usage/MethodUseOfReturn.java
@@ -0,0 +1,11 @@
+public class MethodUseOfReturn {
+    public static int sum(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+    
+    public static void main(String args[]) {
+      int result = sum(10, 20);
+      System.out.println("The sum is: " + result);
+    }
+}
